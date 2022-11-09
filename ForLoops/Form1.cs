8e09ddce91a9a99fe462bae35527c83187c0de51
jasmjosa382 <jasmjosa382@ed.amdsb.ca>
2022-11-09 11:28:05 -0500

    In Progress

--- ForLoops/Form1.cs
+++ ForLoops/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace ForLoops
 {
@@ -23,19 +24,33 @@ namespace ForLoops
 
             for (int i = 1; i <= 5; i++)
             {
+                //Thread.Sleep(100);
+                //Refresh();
+
+                q1Output.Text += $"{i} ";
                 
             }
 
 
             ///2.  Create a loop that displays all values inclusive between 10-6.
+            for (int i = 10; i >= 6; i--)
+            {
+                q2Output.Text += $"{i} ";
 
+            }
 
             ///3.  Create a loop that displays even numbers inclusive between 2 and 10
-
+            for (int i = 2; i <= 10; i = i +2)
+            {
+                q3Output.Text += $"{i} ";
+            }
 
             ///4.  Create a loop that displays the sum of all numbers inclusive 
             ///between 1 and 10. The answers should be 55.
-
+            for (int i = 1; i <= 10; i = i +2)
+            {
+                q4Output.Text += $"{i} ";
+            }
 
             ///5. Create a loop that displays all leap years inclusive between 
             ///1990 and 2010. The output should be: 1992 1996 2000 2004 2008
